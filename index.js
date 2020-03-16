bb832993f1b179a8fabb7f895ee749b23f75617b
Kayla Dailey <kayladailey000@gmail.com>
2020-03-16 10:37:14 -0500

    Post complete

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require ('express');
 const server = express();
 const postRoutes = require('./posts/postRoutes');
 
-
-
- server.use ('/api', postRoutes);
+server.use(express.json());
+server.use ('/api', postRoutes);
 
 
 server.listen(8000, () => console.log('API running on port 8000'));
--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -1,42 +1,100 @@
 const express = require ('express');
 const router = express.Router();
+const Posts = require('../data/db');
+const Comments = require ("../data/db");
+
+
 
 
 //GETS
 router.get ('/', (req, res) =>{
     res.status(200).send('Hello Server test')
 });
+router.get ('/posts', (req, res) =>{
+    Posts.find().then (posts =>{
+            res.status(200).json(posts);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({errorMessage: "It's not you, it's me"})
+    });
 
+})
 router.get('/posts/:id',  (req, res) =>{
-    res.status(200).send(`hello from /GET /posts endpoint`)
-});
-
+    const id = req.params.id;
+        Posts.findById(id).then (postsId=>{
+                    res.status(200).json(postsId);
+            }).catch(err => {
+                res.status(500).json({errorMessage: "It's not you, it's me"})
+            });
+        })
 router.get('/posts/:id/comments', (req, res) =>{
     const id = req.params.id;
-    res.status(200).send(`hello from /GET /posts ${id} endpoint`)
-});
+    Comments.findPostComments(id).then (comments =>{
+                res.status(200).json(comments);
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({errorMessage: "It's not you, it's me"})
+        });
+    })
+
 
 //POSTS
 router.post('/posts', (req, res) =>{
-    res.status(200).send(`hello from /POST /posts endpoint`)
+    const postInfo = req.body;
+
+    if (!postInfo.title || !postInfo.contents) {
+        res.status(400).json({
+            success: false,
+            errorMessage: "Please provide title and contents for the post"
+        });
+    } else {
+    Posts
+    .insert(postInfo)
+    .then (response =>{
+        if (response.id) {
+        res.status(201).json({ ...postInfo, id: response.id });
+} else {
+    res.status(500).json({
+        success: false,
+        errorMessage:
+            "There was an error: It's not you, it's me"
+    });
+}})
+.catch(err => {
+res.status(500).json({
+    success: false,
+    error: err,
+    errorMessage:
+        "There was an error: It's not you, it's me"
+});
+});
+}});
+
+router.post("/posts/:id/comments", (req, res) => {
+    const  id  = req.params;
+    const comment = { ...req.body, post_id: id };
+
+    if (!comment.text) {
+        res.status(400).json({
+            success: false,
+            errorMessage: "Please provide text for the comment"
+        });
+    } else {
+        posts
+            .insertComment(comment)
+            .then(response => {
+                res.status(201).json({ ...comment, id: response.id });
+            })
+            .catch(error => {
+                res.status(404).json({
+                    success: false,
+                    errorMessage:
+                        "The post with the specified ID does not exist"
+                });
+            });
+    }
 });
 
-router.post('/posts/:id/comments', (req, res) =>{
-    const id = req.params.id;
-    res.status(200).send(`hello from /POST /posts ${id} endpoint`)
-});
-
-//DELETE
-router.delete('/posts/:id', (req, res) =>{
-    const id = req.params.id;
-    res.status(200).send(`hello from /DELETE /posts ${id} endpoint`)
-});
-
-//PUT
-router.put('/posts/:id', (req, res) =>{
-    const id = req.params.id;
-    res.status(200).send(`hello from /PUT /posts ${id} endpoint`)
-});
 
 
 module.exports = router;
